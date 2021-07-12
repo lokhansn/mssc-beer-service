3687e019f6a8c6005d0c62f68f9b143a468f8ffc
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-13 00:38:00 +0530

    Initial Project Add mssc-beer-service

---
+++ src/main/java/com/sgl/msscbeerservice/MsscBeerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sgl.msscbeerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerServiceApplication.class, args);
+    }
+
+}
