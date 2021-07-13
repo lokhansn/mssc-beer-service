f5a2a551eaac25e6b685309f9829be45ba402f70
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-13 18:20:11 +0530

    Added Bootstrap class to initialize beer object, closes #6

---
+++ src/main/java/com/sgl/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,47 @@
+package com.sgl.msscbeerservice.bootstrap;
+
+import com.sgl.msscbeerservice.domain.Beer;
+import com.sgl.msscbeerservice.repositories.BeerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Slf4j
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+    @Override
+    public void run(String... args) {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count()==0){
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(100)
+                    .minOnHand(12)
+                    .upc(337010000001L)
+                    .price(new BigDecimal("12.95"))
+                    .build());
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(337010000002L)
+                    .price(new BigDecimal("11.95"))
+                    .build());
+        }
+        log.info("Loaded Beer: "+ beerRepository.count());
+    }
+}
