638b5b55adb9fbefbf8179c24c04444028fa77e0
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-14 10:00:17 +0530

    Added Bean Validation to BeanDto, closes #7 Added Validation and Error handling to Controller, closes #8

--- src/test/java/com/sgl/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/com/sgl/msscbeerservice/web/controller/BeerControllerTest.java
@@ -1,7 +1,9 @@
 package com.sgl.msscbeerservice.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sgl.msscbeerservice.bootstrap.BeerLoader;
 import com.sgl.msscbeerservice.web.model.BeerDto;
+import com.sgl.msscbeerservice.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @WebMvcTest(BeerController.class)
@@ -30,7 +33,7 @@ class BeerControllerTest {
 
     @Test
     void saveNewBeer() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/beer/")
@@ -41,12 +44,24 @@ class BeerControllerTest {
 
     @Test
     void updateBeerById() throws Exception {
-        BeerDto beerDto = BeerDto.builder().build();
+        BeerDto beerDto = getValidBeerDto();
         String beerDtoJson = objectMapper.writeValueAsString(beerDto);
 
         mockMvc.perform(MockMvcRequestBuilders.put("/api/v1/beer/" + UUID.randomUUID().toString())
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(beerDtoJson))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    BeerDto getValidBeerDto(){
+        // If we pass id and version the test case will fail as per the requirement
+        return BeerDto.builder()
+//                .id(UUID.randomUUID())
+//                .version(1)
+                .beerName("Kingfisher")
+                .beerStyle(BeerStyleEnum.ALE)
+                .price(new BigDecimal("2.99"))
+                .upc(12314L)
+                .build();
     }
 }
